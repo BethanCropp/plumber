ae184c5aa531a218d29b7963cd9ac22d9d1286da
BethanCropp <43740410+BethanCropp@users.noreply.github.com>
2022-03-01 16:09:10 +0100

    Create api.R

---
+++ api.R
@@ -0,0 +1,11 @@
+# api.R 
+library(plumber)
+
+#* @get /add
+add <- function(x, y){
+  return(as.numeric(x) + as.numeric(y))
+}
+#* @get /add2
+add2 <- function(x, y){ 
+  list(result = as.numeric(x) + as.numeric(y))
+}
