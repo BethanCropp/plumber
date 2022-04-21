fdf539febf4501d3824d5ba095b34b14535ce8c7
BethanCropp <43740410+BethanCropp@users.noreply.github.com>
2022-04-21 14:08:11 +0200

    Create api.R

---
+++ test2/api.R
@@ -0,0 +1,16 @@
+# api.R 
+library(plumber)
+library(reticulate)
+library(tensorflow)
+library(keras)
+
+keras::dataset_mnist()
+
+#* @get /add
+add <- function(x, y){
+  return(as.numeric(x) + as.numeric(y))
+}
+#* @get /add2
+add2 <- function(x, y){ 
+  list(result = as.numeric(x) + as.numeric(y))
+}
